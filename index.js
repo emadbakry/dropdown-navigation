e8a58ed5f5f5c652497807c06a4ef5fdd1869f17
emadbakry <101658737+emadbakry@users.noreply.github.com>
2022-12-01 17:27:05 +0300

    fix-hover

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// sidebar open and close
 let nav = document.querySelector('nav');
 let burger_icon = document.querySelector('.threeDots');
 
@@ -8,3 +8,14 @@ burger_icon.addEventListener('click', () => {
     burger_icon.toggleAttribute('close_icon');
 });
     
+document.querySelector('section').addEventListener('click',()=>toggle_class('remove'));
+let toggle_class = (ele) => {
+    ele == 'remove'
+         ? sub_lis.forEach((el)=>el.classList.remove('clicked'))
+        : ele.classList.toggle("clicked");
+}
+// show ul on click
+let sub_lis = document.querySelectorAll('.arrow');
+sub_lis.forEach((ele) => ele.addEventListener('click', () => toggle_class(ele)));
+
+// body onclick to hide sub_lis
